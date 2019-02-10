a8db48b255aefe19ba113e6143365edf8631f4c5
Cupaiuoli <lautaro.cupaiuoli@gmail.com>
2019-02-10 03:06:40 -0300

    K-Means - Find closest centroids

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -10,21 +10,32 @@ K = size(centroids, 1);
 
 % You need to return the following variables correctly.
 idx = zeros(size(X,1), 1);
-
+   
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
 %               closest to example i. Hence, it should be a value in the 
 %               range 1..K
-%
+%  
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+for i = 1 : size(X,1)
 
+    maxDist = 99999;
 
+    for j = 1 : K
+        diff2Cen = abs(X(i,:) - centroids(j,:)).^2
+        dist = sum(diff2Cen);
 
+        if dist < maxDist
+            idx(i) = j
+            maxDist = dist
+        end
+    end
 
+end
 
 
 % =============================================================
